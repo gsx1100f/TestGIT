c0bf79a2da7302914f71c44f46c39e9a5a658197
gsx1100f <gsx1100f@hotmail.com>
2022-01-25 12:05:20 +0200

    These are functions in subfolder added to the entire project

---
+++ ForSupport/ForSupport1.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ForSupport1(inputArg1,inputArg2)
+%FORSUPPORT1 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
